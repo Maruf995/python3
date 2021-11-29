8dd6be996b77465b9ad44efbc89e662746486d27
Maruf995 <82045849+Maruf995@users.noreply.github.com>
2021-11-29 18:32:56 +0600

    f

--- post/models.py
+++ post/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class BlogPost(models.Model):
     image = models.FileField(upload_to="post_images/")
     title = models.CharField(max_length=120)
@@ -8,5 +7,16 @@ class BlogPost(models.Model):
     likes = models.IntegerField(default=0)
     reposts = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.title[:20]
+
     def get_absolute_url(self):
-        return f"/blog/{self.pk}"
+        return f"/blog/{self.pk}/"
+
+class Comment(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=150)
+    blog = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+
+    # def __str__(self):
+
--- post/urls.py
+++ post/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
-    #hello_world_view, date_view, blog_view, post_detail, CreateBlogView
-
 
 urlpatterns = [
     path("hello", views.hello_world_view),
     path("date/", views.date_view),
     path("", views.blog_view),
-    path("<int:pk>/", views.post_detail),
-    path("create/", views.CreateBlogView.as_view())
+    path("<int:pk>/", views.post_detail, name='post-detail'),
+    path("<int:pk>/change/", views.EditBlogView.as_view()),
+    path("<int:pk>/comment/", views.create_comment),
+    path("create/", views.CreateBlogView.as_view()),
 ]
+
+
+
+
+
+
